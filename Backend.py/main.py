154c16519526c09e2c04c2a2e2ef2709cbb3032b
shreya-2907 <146342404+shreya-2907@users.noreply.github.com>
2026-08-22 22:13:18 +0530

    cors setup

--- Backend.py/main.py
+++ Backend.py/main.py
@@ -3,6 +3,7 @@ from sqlalchemy import text
 from sqlalchemy.ext.asyncio import AsyncSession
 from db import get_db
 from user import router as user_router
+from fastapi.middleware.cors import CORSMiddleware
 
 import reports
 import user
@@ -10,6 +11,14 @@ import incident
 import storage
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],        # hackathon ke liye sab allow
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(user_router, tags=["users"])
 
 app.include_router(reports.router)
